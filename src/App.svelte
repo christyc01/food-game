47601ada37cb8795dbb40b3d54661011c1d86601
Christy C <57055910+christyc01@users.noreply.github.com>
2024-02-27 15:38:38 -0700

    Add question with buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,22 @@
+<script>
+  const handleClickA = () => {
+    alert('Correct! Food is something to eat.');
+  }
+  const handleClickB = () => {
+    alert('Incorrect! Food is something to eat.');
+  }
+</script>
+
 <main>
-  <h1>Food Game</h1>
   <div class="image"></div>
+  <div class="popup-question">
+    <h2>Question 1</h2>
+    <p>What even is food?</p>
+    <div class="buttons">
+      <button on:click={handleClickA}>Something to eat</button>
+      <button on:click={handleClickB}>Something to post pictures of on Instagram</button>
+    </div>
+  </div>
 </main>
 
 <style>
@@ -11,12 +27,21 @@
     z-index: -2;
   }
   main {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      z-index: 1;
-    }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+  button {
+    flex-grow: 1;
+    background-color: #C4D6D3;
+    flex-basis: 0;
+  }
+  .buttons {
+    display: flex;
+    gap: 10px;
+  }
   .image {
     position: absolute;
     height: 100%;
@@ -26,5 +51,16 @@
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
+  }
+  .popup-question {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #FFA68A;
+    opacity: 0.95;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
   }
 </style>
