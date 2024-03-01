fd94e63f2889de2d4c1f54d3d9bb8b2e7ccbf417
Christy C <57055910+christyc01@users.noreply.github.com>
2024-02-29 19:08:55 -0700

    Move dashboard into its own component, add store

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,27 @@
 <script>
+	import Dashboard from "./components/Dashboard.svelte";
 	import data from "./data/questions-and-answers.json";
-	let count = 0;
+	import { count } from "../src/stores";
 	let questionNumber = 1;
-	let sliderPointer;
 	let showAnswer;
 	let showQuestion;
 	let toggleAnswerOn = false;
 
 	const handleClickA = () => {
-		if (count >= 75) {
+		if ($count >= 75) {
 			alert("You've saved enough money to buy a new car!");
 			return;
 		}
 		toggleAnswerOn = true;
-		count += 5;
+		count.update((n) => n + 5);
 	};
 	const handleClickB = () => {
-		if (count <= -25) {
+		if ($count <= -25) {
 			alert("You've spent too much money on food this month!");
 			return;
 		}
 		toggleAnswerOn = true;
-		count -= 5;
+		count.update((n) => n - 5);
 	};
 
 	const handleClickNext = () => {
@@ -33,7 +33,6 @@
 		}
 	};
 
-	const sliderLinesPositions = [-25, 0, 25, 50, 75];
 	$: {
 		if (toggleAnswerOn && showAnswer && showQuestion) {
 			showAnswer.style.display = "block";
@@ -43,42 +42,11 @@
 			showQuestion.style.display = "block";
 		}
 	}
-
-	$: {
-		if (sliderPointer) {
-			sliderPointer.style.bottom = `${count + 25}%`;
-		}
-	}
 </script>
 
 <main>
 	<div class="screen">
-		<div class="dashboard">
-			<p class="dollars-saved">${count}</p>
-			<p class="dollars-saved-subtext">saved this month</p>
-			<div class="slide-container">
-				<input
-					type="range"
-					min="-25"
-					max="75"
-					value={count}
-					class="slider"
-					id="myRange"
-				/>
-				<div class="slider-pointer" bind:this={sliderPointer}></div>
-				<div class="slider-lines">
-					{#each sliderLinesPositions as position (position)}
-						<div
-							class="slider-lines-and-amounts"
-							style="bottom: {position}%"
-						>
-							<div class="slider-line"></div>
-							<p class="slider-label">${position}</p>
-						</div>
-					{/each}
-				</div>
-			</div>
-		</div>
+		<Dashboard count={$count} />
 		<div class="image-and-questions">
 			<div class="popup-question" bind:this={showQuestion}>
 				<h2>Question {questionNumber}</h2>
@@ -126,21 +94,7 @@
 		display: flex;
 		gap: 10px;
 	}
-	.dashboard {
-		width: 355px;
-		background-color: #485676;
-	}
-	.dollars-saved {
-		color: #ff7c58;
-		font-size: 50px;
-		font-weight: 800;
-		margin: 50px 0px 0px 0px;
-	}
-	.dollars-saved-subtext {
-		color: #fefbda;
-		font-size: 15px;
-		margin: 0px 0px 50px 0px;
-	}
+
 	.image-and-questions {
 		height: 100%;
 		width: 100%;
@@ -175,64 +129,5 @@
 		height: 100%;
 		width: 100%;
 		top: 0;
-	}
-	.slide-container {
-		position: relative;
-		display: flex;
-		justify-content: center;
-		height: 200px;
-	}
-	input[type="range"] {
-		position: absolute;
-		top: 75px;
-		transform: rotate(270deg);
-		height: 5px;
-	}
-	.slider {
-		-webkit-appearance: none;
-		width: 200px;
-		background: #ff7c58;
-		outline: none;
-	}
-	.slider-label {
-		color: #fefbda;
-		font-size: 18px;
-		font-weight: 800;
-		margin: 0;
-		transform: translateY(-10px);
-		padding-left: 10px;
-	}
-	.slider-lines {
-		position: relative;
-		top: -65px;
-		left: 23px;
-		width: 50px;
-	}
-	.slider-line {
-		background-color: #ff7c58;
-		width: 30px;
-	}
-	.slider-lines-and-amounts {
-		display: flex;
-		position: absolute;
-		height: 5px;
-		transform: translateY(-2.5px);
-	}
-	.slider::-webkit-slider-thumb {
-		-webkit-appearance: none;
-		appearance: none;
-	}
-	.slider::-moz-range-thumb {
-		appearance: none;
-	}
-	.slider-pointer {
-		position: absolute;
-		width: 0px;
-		height: 0px;
-		border-left: 25px solid #fefbda;
-		border-bottom: 20px solid transparent;
-		border-right: 25px solid transparent;
-		border-top: 20px solid transparent;
-		margin-right: 10px;
 	}
 </style>
---
+++ src/components/Dashboard.svelte
@@ -0,0 +1,115 @@
+<script>
+	export let count;
+	let sliderPointer;
+	const sliderLinesPositions = [-25, 0, 25, 50, 75];
+
+	$: {
+		if (sliderPointer) {
+			sliderPointer.style.bottom = `${count + 25}%`;
+		}
+	}
+</script>
+
+<div class="dashboard">
+	<p class="dollars-saved">${count}</p>
+	<p class="dollars-saved-subtext">saved this month</p>
+	<div class="slide-container">
+		<input
+			type="range"
+			min="-25"
+			max="75"
+			value={count}
+			class="slider"
+			id="myRange"
+		/>
+		<div class="slider-pointer" bind:this={sliderPointer}></div>
+		<div class="slider-lines">
+			{#each sliderLinesPositions as position (position)}
+				<div
+					class="slider-lines-and-amounts"
+					style="bottom: {position}%"
+				>
+					<div class="slider-line"></div>
+					<p class="slider-label">${position}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	.dashboard {
+		width: 355px;
+		background-color: #485676;
+	}
+	.dollars-saved {
+		color: #ff7c58;
+		font-size: 50px;
+		font-weight: 800;
+		margin: 50px 0px 0px 0px;
+	}
+	.dollars-saved-subtext {
+		color: #fefbda;
+		font-size: 15px;
+		margin: 0px 0px 50px 0px;
+	}
+	.slide-container {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		height: 200px;
+	}
+	input[type="range"] {
+		position: absolute;
+		top: 75px;
+		transform: rotate(270deg);
+		height: 5px;
+	}
+	.slider {
+		-webkit-appearance: none;
+		width: 200px;
+		background: #ff7c58;
+		outline: none;
+	}
+	.slider-label {
+		color: #fefbda;
+		font-size: 18px;
+		font-weight: 800;
+		margin: 0;
+		transform: translateY(-10px);
+		padding-left: 10px;
+	}
+	.slider-lines {
+		position: relative;
+		top: -65px;
+		left: 23px;
+		width: 50px;
+	}
+	.slider-line {
+		background-color: #ff7c58;
+		width: 30px;
+	}
+	.slider-lines-and-amounts {
+		display: flex;
+		position: absolute;
+		height: 5px;
+		transform: translateY(-2.5px);
+	}
+	.slider::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+	}
+	.slider::-moz-range-thumb {
+		appearance: none;
+	}
+	.slider-pointer {
+		position: absolute;
+		width: 0px;
+		height: 0px;
+		border-left: 25px solid #fefbda;
+		border-bottom: 20px solid transparent;
+		border-right: 25px solid transparent;
+		border-top: 20px solid transparent;
+		margin-right: 10px;
+	}
+</style>
